54034c0d35d22b46ef77c3dca8642594d8b0db9e
Ramtin Imani <ramtinimani.dev@gmail.com>
2025-07-25 11:38:56 +0330

    Add note app structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div>test</div>;
+  return (
+    <div className="note-app">
+      <div className="note-app__header">header</div>
+
+      <div className="note-app__body">
+        <div className="note-app__data">add new not form</div>
+        <div className="note-app__container">notes</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
