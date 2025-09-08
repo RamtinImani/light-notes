2f6211de7e81d269529121907de67529389bd8b2
Ramtin Imani <ramtinimani.dev@gmail.com>
2025-09-08 11:19:41 +0330

    Fix: centering modal bug

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,15 +4,16 @@ export default function Modal({ title, open, onOpen, children }) {
   if (!open) return null;
   return (
     <>
-      <div onClick={() => onOpen(false)} className="backdrop"></div>
-      <div className="modal">
-        <div className="modal__header">
-          <h3 className="modal__title">{title}</h3>
-          <button onClick={() => onOpen(false)}>
-            <XCircleIcon className="modal__close" />
-          </button>
+      <div onClick={() => onOpen(false)} className="modal__backdrop">
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+          <div className="modal__header">
+            <h3 className="modal__title">{title}</h3>
+            <button onClick={() => onOpen(false)}>
+              <XCircleIcon className="modal__close" />
+            </button>
+          </div>
+          {children}
         </div>
-        {children}
       </div>
     </>
   );
